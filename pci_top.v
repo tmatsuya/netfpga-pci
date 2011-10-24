3be3f99a2b0504921edc93c09142d843d2cb5373
Takeshi MATSUYA <macchan@sfc.wide.ad.jp>
2011-10-25 01:15:51 +0900

    Added Initiator function codes (WIP) first commit

--- pci_top.v
+++ pci_top.v
@@ -45,24 +45,43 @@ reg PCI_IDSel = 1'b0;
 // Port register
 //-----------------------------------
 reg AD_Hiz = 1'b1;
-reg [31:0] AD_Port = 32'h0;
-reg DEVSEL_Hiz = 1'b1;
-reg DEVSEL_Port = 1'b1;
-reg TRDY_Hiz = 1'b1;
-reg TRDY_Port = 1'b1;
-reg STOP_Hiz = 1'b1;
-reg STOP_Port = 1'b1;
+reg [31:0] AD_Port       = 32'h0;
+reg DEVSEL_Hiz           = 1'b1;
+reg DEVSEL_Port          = 1'b1;
+reg TRDY_Hiz             = 1'b1;
+reg TRDY_Port            = 1'b1;
+reg STOP_Hiz             = 1'b1;
+reg STOP_Port            = 1'b1;
 
-reg LED_Port = 1'b0;
+reg REQ_Port             = 1'b0;
+reg PCIMSTAD_Hiz         = 1'b1;
+wire [31:0] PCIMSTAD_Port;
+reg CBE_Hiz              = 1'b1;
+wire [3:0] CBE_Port;
+reg FRAME_Hiz            = 1'b1;
+reg FRAME_Port           = 1'b1;
+reg IRDY_Hiz             = 1'b1;
+reg IRDY_Port            = 1'b1;
 
-parameter PCI_IDLE		= 3'b000;
-parameter PCI_ADDR_COMPARE	= 3'b001;
-parameter PCI_BUS_BUSY		= 3'b010;
-parameter PCI_WAIT_IRDY		= 3'b011;
-parameter PCI_WAIT_LOCAL_ACK	= 3'b100;
-parameter PCI_ACC_COMPLETE	= 3'b101;
-parameter PCI_DISCONNECT	= 3'b110;
-parameter PCI_TURN_AROUND	= 3'b111;
+//-----------------------------------
+// Initiator register
+//-----------------------------------
+reg MST_Start            = 1'b0;
+reg MST_Busy             = 1'b0;
+reg MST_ReadWrite        = 1'b0;
+reg MST_Abort            = 1'b0;
+reg TGT_Abort            = 1'b0;
+reg MST_IntStat          = 1'b0;
+reg MST_IntClr           = 1'b0;
+reg MST_IntMask          = 1'b0;
+reg [3:0] DEVSEL_Count   = 4'd0;
+reg Retry                = 1'b0;
+
+reg [31:2] MST_Address   = 30'h0;
+reg [31:0] MST_WriteData = 32'h0;
+reg [31:0] MST_ReadData  = 32'h0;
+
+reg LED_Port             = 1'b0;
 
 parameter PCI_IO_CYCLE		= 3'b001;
 parameter PCI_IO_READ_CYCLE	= 4'b0010;
@@ -74,6 +93,24 @@ parameter PCI_CFG_CYCLE		= 3'b101;
 parameter PCI_CFG_READ_CYCLE	= 4'b1010;
 parameter PCI_CFG_WRITE_CYCLE	= 4'b1011;
 
+parameter TGT_IDLE		= 3'b000;
+parameter TGT_ADDR_COMPARE	= 3'b001;
+parameter TGT_BUS_BUSY		= 3'b010;
+parameter TGT_WAIT_IRDY		= 3'b011;
+parameter TGT_WAIT_LOCAL_ACK	= 3'b100;
+parameter TGT_ACC_COMPLETE	= 3'b101;
+parameter TGT_DISCONNECT	= 3'b110;
+parameter TGT_TURN_AROUND	= 3'b111;
+
+parameter INI_IDLE		= 3'b000;
+parameter INI_BUS_PARK		= 3'b001;
+parameter INI_WAIT_GNT		= 3'b010;
+parameter INI_ADDR2DATA		= 3'b011;
+parameter INI_WAIT_DEVSEL	= 3'b100;
+parameter INI_WAIT_COMPLETE	= 3'b101;
+parameter INI_ABORT		= 3'b110;
+parameter INI_TURN_AROUND	= 3'b111;
+
 parameter SEQ_IDLE		= 3'b000;
 parameter SEQ_IO_ACCESS		= 3'b001;
 parameter SEQ_MEM_ACCESS	= 3'b010;
@@ -81,7 +118,8 @@ parameter SEQ_CFG_ACCESS	= 3'b011;
 parameter SEQ_ROM_ACCESS	= 3'b100;
 parameter SEQ_COMPLETE		= 3'b111;
 
-reg [2:0] pci_current_state = PCI_IDLE, pci_next_state = PCI_IDLE;
+reg [2:0] target_current_state = TGT_IDLE, target_next_state = TGT_IDLE;
+reg [2:0] initiator_current_state = INI_IDLE, initiator_next_state = INI_IDLE;
 reg [2:0] seq_current_state = SEQ_IDLE, seq_next_state = SEQ_IDLE;
 
 //-----------------------------------
@@ -140,10 +178,13 @@ rom rom_inst (
 	assign dout[31:0] = 32'hcdab3412;
 `endif
 
+//-----------------------------------
+// Target
+//-----------------------------------
 always @(posedge PCLK) begin
 	if (~RST_I) begin
-		pci_current_state <= PCI_IDLE;
-		pci_next_state <= PCI_IDLE;
+		target_current_state <= TGT_IDLE;
+		target_next_state <= TGT_IDLE;
 		AD_Hiz <= 1'b1;
 		DEVSEL_Hiz <= 1'b1;
 		DEVSEL_Port <= 1'b1;
@@ -156,77 +197,225 @@ always @(posedge PCLK) begin
 		PCI_IDSel <= 1'b0;
 		Local_Bus_Start <= 1'b0;
 	end else begin
-		pci_current_state <= pci_next_state;
-		case (pci_current_state)
-			PCI_IDLE: begin
+		target_current_state <= target_next_state;
+		case (target_current_state)
+			TGT_IDLE: begin
 				if (~FRAME_IO && IRDY_IO) begin
 					PCI_BusCommand <= CBE_IO;
 					PCI_Address <= AD_IO;
 					PCI_IDSel <= IDSEL_I;
-					pci_next_state <= PCI_ADDR_COMPARE;
+					target_next_state <= TGT_ADDR_COMPARE;
 				end
 			end
-			PCI_ADDR_COMPARE: begin
+			TGT_ADDR_COMPARE: begin
 				if (Hit_Device) begin
 					DEVSEL_Port <= 1'b0;
 					DEVSEL_Hiz <= 1'b0;
 					TRDY_Hiz <= 1'b0;
 					STOP_Hiz <= 1'b0;
-					pci_next_state <= PCI_WAIT_IRDY;
+					target_next_state <= TGT_WAIT_IRDY;
 				end else
-					pci_next_state <= PCI_BUS_BUSY;
+					target_next_state <= TGT_BUS_BUSY;
 			end
-			PCI_BUS_BUSY: begin
+			TGT_BUS_BUSY: begin
 				if (FRAME_IO & IRDY_IO)
-					pci_next_state <= PCI_IDLE;
+					target_next_state <= TGT_IDLE;
 			end
-			PCI_WAIT_IRDY: begin
+			TGT_WAIT_IRDY: begin
 				if (~IRDY_IO) begin
 					if (PCI_BusCommand[0] == 1'b0)
 						AD_Hiz <= 1'b0;
 					Local_Bus_Start <= 1'b1;
-					pci_next_state <= PCI_WAIT_LOCAL_ACK;
+					target_next_state <= TGT_WAIT_LOCAL_ACK;
 				end
 			end
-			PCI_WAIT_LOCAL_ACK: begin
+			TGT_WAIT_LOCAL_ACK: begin
 				Local_Bus_Start <= 1'b0;
 				if (Local_DTACK) begin
 					TRDY_Port <= 1'b0;
 					STOP_Port <= 1'b0;
-					pci_next_state <= PCI_ACC_COMPLETE;
+					target_next_state <= TGT_ACC_COMPLETE;
 				end
 			end
-			PCI_ACC_COMPLETE: begin
+			TGT_ACC_COMPLETE: begin
 				TRDY_Port <= 1'b1;
 				AD_Hiz <= 1'b1;
 				if (~FRAME_IO) begin
-					pci_next_state <= PCI_DISCONNECT;
+					target_next_state <= TGT_DISCONNECT;
 				end else begin
 					DEVSEL_Port <= 1'b1;
 					STOP_Port <= 1'b1;
-					pci_next_state <= PCI_TURN_AROUND;
+					target_next_state <= TGT_TURN_AROUND;
 				end
 			end
-			PCI_DISCONNECT: begin
+			TGT_DISCONNECT: begin
 				if (FRAME_IO) begin
 					DEVSEL_Port <= 1'b1;
 					STOP_Port <= 1'b1;
-					pci_next_state <= PCI_TURN_AROUND;
+					target_next_state <= TGT_TURN_AROUND;
 				end
 			end
-			PCI_TURN_AROUND: begin
+			TGT_TURN_AROUND: begin
 				DEVSEL_Hiz <= 1'b1;
 				TRDY_Hiz <= 1'b1;
 				STOP_Hiz <= 1'b1;
-				pci_next_state <= PCI_IDLE;
+				target_next_state <= TGT_IDLE;
 			end
 			default: begin
-				pci_next_state <= PCI_TURN_AROUND;
+				target_next_state <= TGT_TURN_AROUND;
 			end
 		endcase 
 	end
 end
+
+//-----------------------------------
+// Initiator
+//-----------------------------------
+assign PCIMSTAD_Port = ~FRAME_Port ? {MST_Address, 2'b00} : MST_WriteData;
+assign CBE_Port      = ~FRAME_Port ? {PCI_MEM_CYCLE, MST_ReadWrite} : 4'b0000;
+
+always @(posedge PCLK) begin
+	if (~RST_I) begin
+		MST_ReadData <= 32'hffffffff;
+	end else begin
+		if (MST_Abort) begin
+			MST_ReadData <= 32'hffffffff;
+		end else begin
+			if (~MST_ReadWrite & ~IRDY_Port & ~TRDY_IO)
+				MST_ReadData <= AD_IO;
+		end
+	end
+end
+
+always @(posedge PCLK) begin
+	if (~RST_I) begin
+		initiator_current_state <= INI_IDLE;
+		initiator_next_state <= INI_IDLE;
+	end else begin
+		initiator_current_state <= initiator_next_state;
+		case (target_current_state)
+			INI_IDLE: begin
+				if (CFG_Cmd_Mst) begin
+					if (MST_Start | Retry) begin
+						MST_Busy <= 1'b1;
+						if (~GNT_I & FRAME_IO & IRDY_IO) begin
+							PCIMSTAD_Hiz <= 1'b0;
+							CBE_Hiz     <= 1'b0;
+							FRAME_Port   <= 1'b0;
+							FRAME_Hiz    <= 1'b0;
+							initiator_next_state <= INI_ADDR2DATA;
+						end else begin
+							REQ_Port <= 1'b0;
+							initiator_next_state <= INI_WAIT_GNT;
+						end
+					end else begin
+						if (~GNT_I) begin
+							PCIMSTAD_Hiz <= 1'b0;
+							CBE_Hiz     <= 1'b0;
+						end else begin
+							PCIMSTAD_Hiz <= 1'b1;
+							CBE_Hiz     <= 1'b1;
+						end
+					end
+				end else begin
+					if (MST_Start) begin
+						MST_Busy <= 1'b1;
+						initiator_next_state <= INI_TURN_AROUND;
+					end
+				end
+			end
+			INI_WAIT_GNT: begin
+				if (~GNT_I & FRAME_IO & IRDY_IO) begin
+					REQ_Port     <= 1'b1;
+					PCIMSTAD_Hiz <= 1'b0;
+					CBE_Hiz      <= 1'b0;
+					FRAME_Port   <= 1'b0;
+					FRAME_Hiz    <= 1'b0;
+					initiator_next_state <= INI_ADDR2DATA;
+				end
+			end
+			INI_ADDR2DATA: begin
+				FRAME_Port  <= 1'b0;
+				IRDY_Port   <= 1'b0;
+				IRDY_Hiz    <= 1'b0;
+				if (~MST_ReadWrite)
+					PCIMSTAD_Hiz <= 1'b1;
+				TGT_Abort <= 1'b0;
+				MST_Abort <= 1'b0;
+				initiator_next_state <= INI_WAIT_DEVSEL;
+			end
+			INI_WAIT_DEVSEL: begin
+				if (~DEVSEL_IO) begin
+					if (~TRDY_IO) begin
+						FRAME_Hiz    <= 1'b1;
+						IRDY_Port    <= 1'b1;
+						PCIMSTAD_Hiz <= 1'b1;
+						CBE_Hiz      <= 1'b1;
+						MST_Busy     <= 1'b0;
+						initiator_next_state <= INI_TURN_AROUND;
+					end else begin
+						if (~STOP_IO)
+							initiator_next_state <= INI_ABORT;
+						else
+							initiator_next_state <= INI_WAIT_COMPLETE;
+					end
+				end else begin
+					if (DEVSEL_Count == 4'h03) begin
+						MST_Abort <= 1'b1;
+						initiator_next_state <= INI_ABORT;
+					end else begin
+						DEVSEL_Count <= DEVSEL_Count + 1;
+					end
+				end
+			end
+			INI_WAIT_COMPLETE: begin
+				if (~DEVSEL_IO) begin
+					if (~TRDY_IO) begin
+						FRAME_Hiz    <= 1'b1;
+						IRDY_Port    <= 1'b1;
+						PCIMSTAD_Hiz <= 1'b1;
+						CBE_Hiz      <= 1'b1;
+						MST_Busy     <= 1'b0;
+						initiator_next_state <= INI_TURN_AROUND;
+					end else
+						if (~STOP_IO)
+							initiator_next_state <= INI_ABORT;
+				end else begin
+					if (~STOP_IO) begin
+						TGT_Abort <= 1'b1;
+					end else begin
+						MST_Abort <= 1'b1;
+						TGT_Abort <= 1'b1;
+					end
+					initiator_next_state <= INI_ABORT;
+				end
+			end
+			INI_ABORT: begin
+				FRAME_Hiz    <= 1'b1;
+				IRDY_Port    <= 1'b1;
+				PCIMSTAD_Hiz <= 1'b1;
+				CBE_Hiz      <= 1'b1;
+				if (TGT_Abort | MST_Abort)
+					MST_Busy <= 1'b0;
+				else
+					Retry <= 1'b1;
+				initiator_next_state <= INI_TURN_AROUND;
+			end
+			INI_TURN_AROUND: begin
+				IRDY_Hiz     <= 1'b1;
+				DEVSEL_Count <= 0;
+				initiator_next_state <= INI_IDLE;
+			end
+			default:
+				initiator_next_state <= INI_IDLE;
+		endcase
+	end
+end
+
 					 
+//-----------------------------------
+// Sequencer
+//-----------------------------------
 always @(posedge PCLK) begin
 	if (~RST_I) begin
 		seq_current_state <= SEQ_IDLE;
@@ -261,9 +450,53 @@ always @(posedge PCLK) begin
 			end
 			SEQ_IO_ACCESS: begin
 				if (~PCI_BusCommand[0]) begin
-					AD_Port[31:0] <= 32'hcdab3412;
+					case (PCI_Address[4:2])
+						3'b000:
+							AD_Port[31:0] <= {MST_IntStat,MST_IntMask,14'b0,MST_Abort,TGT_Abort,12'b0,MST_ReadWrite,MST_Busy};
+						3'b001:
+							AD_Port[31:0] <= {MST_Address, 2'b00};
+						3'b010:
+							AD_Port[31:0] <= MST_ReadData;
+						3'b011:
+							AD_Port[31:0] <= MST_WriteData;
+						default:
+							AD_Port[31:0] <= 32'hcdab3412;
+					endcase
 				end else begin
-					LED_Port <= AD_IO[0];
+					case (PCI_Address[4:2])
+						3'b000: begin
+							if (~CBE_IO[3]) begin
+								MST_IntClr  <= AD_IO[31];
+								MST_IntMask <= AD_IO[30];
+							end
+							if (~CBE_IO[0]) begin
+								MST_ReadWrite <= AD_IO[1];
+								MST_Start     <= AD_IO[0];
+							end
+						end
+						3'b001: begin
+							if (~CBE_IO[3])
+								MST_Address[31:24] <= AD_IO[31:24];
+							if (~CBE_IO[2])
+								MST_Address[23:16] <= AD_IO[23:16];
+							if (~CBE_IO[1])
+								MST_Address[15: 8] <= AD_IO[15: 8];
+							if (~CBE_IO[0])
+								MST_Address[ 7: 2] <= AD_IO[ 7: 2];
+						end
+						3'b011: begin
+							if (~CBE_IO[3])
+								MST_WriteData[31:24] <= AD_IO[31:24];
+							if (~CBE_IO[2])
+								MST_WriteData[23:16] <= AD_IO[23:16];
+							if (~CBE_IO[1])
+								MST_WriteData[15: 8] <= AD_IO[15: 8];
+							if (~CBE_IO[0])
+								MST_WriteData[ 7: 2] <= AD_IO[ 7: 2];
+						end
+						default:
+							LED_Port <= AD_IO[0];
+					endcase
 				end
 				Local_DTACK <= 1'b1;
 				seq_next_state <= SEQ_COMPLETE;
@@ -271,9 +504,8 @@ always @(posedge PCLK) begin
 			SEQ_MEM_ACCESS: begin
 				if (~PCI_BusCommand[0]) begin
 					AD_Port[31:0] <= dout[31:0];
-				end else begin
+				end else
 					LED_Port <= AD_IO[0];
-				end
 				Local_DTACK <= 1'b1;
 				seq_next_state <= SEQ_COMPLETE;
 			end
@@ -304,34 +536,20 @@ always @(posedge PCLK) begin
 							AD_Port[15:8]  <= CFG_ProgramIF;
 							AD_Port[7:0]   <= CFG_RevisionID;
 						end
-						6'b000011: begin	// Header Type/other
-							AD_Port[31:24] <= 8'b0;
-							AD_Port[23:16] <= CFG_HeaderType;
-							AD_Port[15:0]  <= 16'b0;
-						end
-						6'b000100: begin	// Base Addr Register 0
-							AD_Port[31:24] <= CFG_Base_Addr0;
-							AD_Port[23:0]  <= 24'b0;
-						end
-						6'b000101: begin	// Base Addr Register 1
+						6'b000011: 		// Header Type/other
+							AD_Port[31:0]  <= {8'b0, CFG_HeaderType, 16'b0};
+						6'b000100: 		// Base Addr Register 0
+							AD_Port[31:0]  <= {CFG_Base_Addr0, 24'b0};
+						6'b000101: 		// Base Addr Register 1
 							AD_Port[31:0]  <= {16'h0, CFG_Base_Addr1, 5'b00001};
-						end
-						6'b001011: begin	// Sub System Vendor/Sub System ID
-							AD_Port[31:16] <= CFG_DeviceID;
-							AD_Port[15:0]  <= CFG_VendorID;
-						end
+						6'b001011:		// Sub System Vendor/Sub System ID
+							AD_Port[31:0]  <= {CFG_DeviceID, CFG_VendorID};
 `ifdef ENABLE_EXPROM
-						6'b001100: begin	// Exp ROM Base Addr
-							AD_Port[31:20] <= CFG_ExpROM_Addr;
-							AD_Port[19:1]  <= 19'b0;
-							AD_Port[0]     <= CFG_ExpROM_En;
-						end
+						6'b001100: 		// Exp ROM Base Addr
+							AD_Port[31:0]  <= {CFG_ExpROM_Addr, 19'b0, CFG_ExpROM_En};
 `endif
-						6'b001111: begin	// Interrupt Register
-							AD_Port[31:16] <= 16'b0;
-							AD_Port[15:8]  <= CFG_Int_Pin;
-							AD_Port[7:0]   <= CFG_Int_Line;
-						end
+						6'b001111:		// Interrupt Register
+							AD_Port[31:0]  <= {16'b0, CFG_Int_Pin, CFG_Int_Line};
 						default:
 							AD_Port[31:0]  <= 32'h0;
 					endcase
@@ -403,16 +621,16 @@ always @(posedge PCLK) begin
 	end
 end
 
-assign CBE_IO = 4'hz;
-assign AD_IO = AD_Hiz ? 32'hz : AD_Port;
-assign PAR_IO = 1'hz;
-assign FRAME_IO = 1'hz;
-assign IRDY_IO = 1'hz;
-assign TRDY_IO = TRDY_Hiz ? 1'hz : TRDY_Port;
-assign STOP_IO = STOP_Hiz ? 1'hz : STOP_Port;
-assign DEVSEL_IO = DEVSEL_Hiz ? 1'hz : DEVSEL_Port;
-assign INTA_O = 1'hz;
-assign REQ_O = 1'hz;
-assign LED = ~LED_Port;
+assign CBE_IO    = CBE_Hiz   ? 4'hz : CBE_Port;
+assign AD_IO     = AD_Hiz    ?32'hz : AD_Port;
+assign PAR_IO    = 1'hz;
+assign FRAME_IO  = FRAME_Hiz ? 1'hz : FRAME_Port;
+assign IRDY_IO   = IRDY_Hiz  ? 1'hz : IRDY_Port;
+assign TRDY_IO   = TRDY_Hiz  ? 1'hz : TRDY_Port;
+assign STOP_IO   = STOP_Hiz  ? 1'hz : STOP_Port;
+assign DEVSEL_IO = DEVSEL_Hiz? 1'hz : DEVSEL_Port;
+assign INTA_O    = 1'hz;
+assign REQ_O     = REQ_Port;
+assign LED       = ~LED_Port;
 
 endmodule
